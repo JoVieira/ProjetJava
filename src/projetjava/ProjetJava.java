ab34f8dd44ba606e07481133dfd49110c160c4f1
Joel <joel.vieira@edu.ece.fr>
2018-04-16 15:27:55 +0200

    add jframe
    add jframe

--- src/projetjava/ProjetJava.java
+++ src/projetjava/ProjetJava.java
@@ -5,19 +5,39 @@
  */
 package projetjava;
 
+import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.event.*;
 /**
  *
  * @author joel
  */
-public class ProjetJava {
-
-    /**
-     * @param args the command line arguments
-     */
+public class ProjetJava extends JFrame implements ActionListener {
+    
+    private JPanel pan;
+    private JButton bout;
+    
+    public ProjetJava() {
+        setTitle("Hopital");
+        setSize(600,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pan = new JPanel();
+        getContentPane().add(pan);
+        this.setLayout(new BorderLayout());
+        bout = new JButton("coucou"); 
+        add(bout, "North");
+        bout.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed (ActionEvent e) {
+        bout.setText("salut");
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("BarJo c le + bo");
-        int hh;
+        ProjetJava pj = new ProjetJava();
+        pj.setVisible(true);
+
     }
     
 }
